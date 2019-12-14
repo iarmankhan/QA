018762e381bac4d839a352ea174993e8210b2a0f
iarmankhan <armankhan9244@gmail.com>
2019-12-15 00:40:19 +0530

    Pre-populated database
    -Created question factory
    -Created database seeder function

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Question;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class, 3)->create()->each(function ($u){
+            $u->questions()
+                ->saveMany(
+                    factory(Question::class, rand(1, 5))->make()
+                );
+        });
     }
 }
