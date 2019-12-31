a653cc9a76a69660914e3b7392f2c187957aa41e
iarmankhan <armankhan9244@gmail.com>
2019-12-31 23:52:47 +0530

    Generated Fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use function foo\func;
 
 class Answer extends Model
 {
@@ -19,5 +20,16 @@ class Answer extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($answer){
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+
     }
 }
