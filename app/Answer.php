9ec7e47f4e7b2c19c7fad432c47c4dcacfc6973a
iarmankhan <armankhan9244@gmail.com>
2020-01-12 15:01:04 +0530

    Securing the app from xss attacks

--- app/Answer.php
+++ app/Answer.php
@@ -13,7 +13,7 @@ class Answer extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean(\Parsedown::instance()->text($this->body));
     }
 
     public function question()
--- app/Question.php
+++ app/Question.php
@@ -45,7 +45,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers()
@@ -76,5 +76,20 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites()->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length = 100)
+    {
+        return Str::limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
