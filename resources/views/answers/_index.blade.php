41d6dff2f60a3df6af80529a05199efbf53bf9e9
iarmankhan <armankhan9244@gmail.com>
2020-01-12 15:58:39 +0530

    Tidy up views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,45 +10,7 @@
                     @include('layouts._messages')
 
                     @foreach($answers as $answer)
-                        <div class="media">
-                            @include('shared._vote', [
-                                   'model'=> $answer
-                            ])
-                            <div class="media-body">
-                                {!! $answer->body_html !!}
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="ml-auto">
-                                            @can('update', $answer)
-                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}"
-                                                   class="btn btn-sm btn-outline-info">Edit</a>
-                                            @endcan
-
-                                            @can('delete', $answer)
-                                                <form class="form-delete"
-                                                      action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}"
-                                                      method="POST">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button class="btn btn-outline-danger btn-sm"
-                                                            onclick="confirm('Are you sure?')">Delete
-                                                    </button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <div class="col-4"></div>
-                                    <div class="col-4">
-                                        @include('shared._author', [
-                                            'model' => $answer,
-                                            'label' => 'Answered'
-                                        ])
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <hr>
+                        @include('answers._answer')
                     @endforeach
                 </div>
             </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -9,7 +9,8 @@
                         <div class="d-flex align-items-center">
                             <h3>All Questions</h3>
                             <div class="ml-auto">
-                                <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask a Question</a>
+                                <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask a
+                                    Question</a>
                             </div>
                         </div>
 
@@ -18,57 +19,16 @@
 
                     <div class="card-body">
                         @include('layouts._messages')
-                      @forelse($questions as $question)
-                            <div class="media">
-                                <div class="d-flex flex-column counters">
-                                    <div class="vote">
-                                        <strong>{{ $question->votes_count }}</strong> {{ Str::plural('vote', $question->votes_count) }}
-                                    </div>
-                                    <div class="status {{ $question->status }}">
-                                        <strong>{{ $question->answers_count }}</strong> {{ Str::plural('answer', $question->answers_count) }}
-                                    </div>
-                                    <div class="view">
-                                        {{ $question->views . " " . Str::plural('view', $question->views) }}
-                                    </div>
-                                </div>
-                                <div class="media-body">
-                                    <div class="d-flex align-items-center">
-                                        <h3 class="mt-0">
-                                            <a href="{{ $question->url }}">{{ $question->title }}</a>
-                                        </h3>
-                                        <div class="ml-auto">
-                                            @can('update', $question)
-                                                <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            @endcan
-
-                                            @can('delete', $question)
-                                                <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="POST">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button class="btn btn-outline-danger btn-sm" onclick="confirm('Are you sure?')">Delete</button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <p class="lead">
-                                        Asked by
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                        <small class="text-muted">{{ $question->created_date }}</small>
-                                    </p>
-                                    <div class="excerpt">
-                                        {{ $question->excerpt }}
-                                    </div>
-                                </div>
-                            </div>
-                            <hr>
+                        @forelse($questions as $question)
+                            @include('questions._question')
                         @empty
                             <div class="alert alert-warning">
                                 <strong>Sorry</strong> There are no questions available!
                             </div>
                         @endforelse
-                        <div class="d-flex justify-content-center">
-                            {{ $questions->links() }}
-                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        {{ $questions->links() }}
                     </div>
                 </div>
             </div>
